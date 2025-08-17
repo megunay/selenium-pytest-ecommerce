7c8bb58f7fa315c6583fc8c66f7f8b0312f78077
Musteba Erkan GUNAY <merkang91@gmail.com>
2025-08-17 22:15:30 +0700

    Test Cases

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,1 +1,21 @@
+from utils.driver_factory import get_driver
+from pages.login_page import LoginPage
+from pages.products_page import ProductsPage
+import pytest
+
+@pytest.fixture
+def setup():
+    driver = get_driver()
+    yield driver
+    driver.quit()
+
+def test_checkout_flow(setup):
+    login = LoginPage(setup)
+    login.load()
+    login.login("standard_user", "secret_sauce")
+    products = ProductsPage(setup)
+    products.add_first_product_to_cart()
+    products.go_to_cart()
+    # Add assertions here to validate cart (extend later)
+    assert "cart" in setup.current_url
 
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,1 +1,23 @@
+import pytest
+from utils.driver_factory import get_driver
+from pages.login_page import LoginPage
+from pages.products_page import ProductsPage
 
+@pytest.fixture
+def setup():
+    driver = get_driver()
+    yield driver
+    driver.quit()
+
+def test_valid_login(setup):
+    login = LoginPage(setup)
+    login.load()
+    login.login("standard_user", "secret_sauce")
+    products = ProductsPage(setup)
+    assert products.get_title() == "Products"
+
+def test_invalid_login(setup):
+    login = LoginPage(setup)
+    login.load()
+    login.login("wrong_user", "wrong_pass")
+    assert "Username and password" in login.get_error_message()
